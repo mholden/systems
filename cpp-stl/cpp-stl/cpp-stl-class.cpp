fe1c1b71fe09dc47e54a0ab8e559f6955ddce361
Max Holden <maxholden11@gmail.com>
2022-03-14 09:53:07 -0400

    cpp-stl
    -sets, maps, unordered maps working with custom Key, Value
    classes

--- cpp-stl/cpp-stl/cpp-stl-class.cpp
+++ cpp-stl/cpp-stl/cpp-stl-class.cpp
@@ -7,12 +7,59 @@
 #include <map>
 #include <unordered_map>
 
+namespace myns {
+
+class Key {
+public:
+    int k_key;
+    
+    Key(int);
+    
+    bool operator==(const Key& k2) const {
+        if (this->k_key == k2.k_key)
+            return true;
+        else
+            return false;
+    }
+    
+    bool operator<(const Key& k2) const {
+        return this->k_key < k2.k_key;
+    }
+};
+
+Key::Key(int k) {
+    this->k_key = k;
+}
+
+class Value {
+public:
+    int v_value;
+    Value(int);
+};
+
+Value::Value(int v) {
+    this->v_value = v;
+}
+
+}
+
+// specialize std::hash for our class
+namespace std {
+template<>
+class hash<myns::Key> {
+public:
+    size_t operator()(const myns::Key& k) const {
+        return std::hash<int>()(k.k_key);
+    }
+};
+}
+
 int main(int argc, char **argv) {
     int err = 0, num;
-    std::unordered_set<int> keys;
-    std::map<int, int> m;
-    std::unordered_map<int, int> um;
-    std::chrono::high_resolution_clock::time_point tstart, tend;
+    std::unordered_set<myns::Key> keys;
+    std::map<myns::Key, myns::Value> m;
+    std::unordered_map<myns::Key, myns::Value> um;
+    std::chrono::steady_clock::time_point tstart, tend;
     std::chrono::duration<double, std::milli> mit(0), mlt(0), umit(0), umlt(0);
     std::chrono::duration<double, std::micro> avg;
     
@@ -32,9 +79,11 @@ int main(int argc, char **argv) {
     //std::cout << "setup: start\n";
     // build a set of 'num' random integers
     for (int i = 0; i < num; i++) {
-        auto [it, success] = keys.insert(rand());
+        myns::Key k(rand());
+        auto [it, success] = keys.insert(k);
         while (!success) {
-            auto [nit, nsuccess] = keys.insert(rand());
+            k.k_key = rand();
+            auto [nit, nsuccess] = keys.insert(k);
             success = nsuccess;
         }
     }
@@ -45,7 +94,7 @@ int main(int argc, char **argv) {
     // dump set
     std::cout << "*** set ***\n";
     for (auto it = begin(keys); it != end(keys); it++) {
-        std::cout << "keys[" << std::distance(keys.begin(), it) << "]: " << *it << std::endl;
+        std::cout << "keys[" << std::distance(keys.begin(), it) << "]: " << it->k_key << std::endl;
     }
 #endif
     
@@ -57,9 +106,11 @@ int main(int argc, char **argv) {
     // inserts
     //std::cout << "maps: starting inserts\n";
     for (auto it = begin(keys); it != end(keys); it++) {
-        tstart = std::chrono::high_resolution_clock::now();
-        auto [iit, success] = m.insert({*it, *it});
-        tend = std::chrono::high_resolution_clock::now();
+        myns::Key k(it->k_key);
+        myns::Value v(it->k_key);
+        tstart = std::chrono::steady_clock::now();
+        auto [iit, success] = m.insert({k, v});
+        tend = std::chrono::steady_clock::now();
         mit += tend - tstart;
         if (!success) {
             std::cout << "m.insert failed\n" << std::endl;
@@ -74,7 +125,7 @@ int main(int argc, char **argv) {
     // dump map
     std::cout << "*** map ***\n";
     for (auto& [k, v] : m) {
-        std::cout << "m[" << k << "]: " << v << std::endl;
+        std::cout << "m[" << k.k_key << "]: " << v.v_value << std::endl;
     }
 #endif
     
@@ -82,7 +133,7 @@ int main(int argc, char **argv) {
     //std::cout << "maps: starting lookups\n";
     for (int i = 0; i < num; i++) {
         auto bno = rand() % keys.bucket_count();
-        while (!keys.bucket_size(bno)) {
+        while (!keys.bucket_size(bno)) { // find a non-empty bucket
             bno++;
             if (bno >= keys.bucket_count())
                 bno = 0;
@@ -90,9 +141,9 @@ int main(int argc, char **argv) {
         auto it = keys.begin(bno);
         std::advance(it, rand() % keys.bucket_size(bno));
         //std::cout << "looking up key " << *it << "\n";
-        tstart = std::chrono::high_resolution_clock::now();
+        tstart = std::chrono::steady_clock::now();
         auto record = m.find(*it);
-        tend = std::chrono::high_resolution_clock::now();
+        tend = std::chrono::steady_clock::now();
         mlt += tend - tstart;
         if (record == m.end()) {
             std::cout << "m.find failed\n" << std::endl;
@@ -104,16 +155,17 @@ int main(int argc, char **argv) {
     avg = mlt / num;
     std::cout << "maps lookup time: " << mlt.count() << "ms" << " avg: " << avg.count() << "us\n";
     
-    
     // *********** unordered maps **********
     std::cout << "*** unordered maps ***\n";
     
     // inserts
     //std::cout << "umaps: starting inserts\n";
     for (auto it = begin(keys); it != end(keys); it++) {
-        tstart = std::chrono::high_resolution_clock::now();
-        auto [iit, success] = um.insert({*it, *it});
-        tend = std::chrono::high_resolution_clock::now();
+        myns::Key k(it->k_key);
+        myns::Value v(it->k_key);
+        tstart = std::chrono::steady_clock::now();
+        auto [iit, success] = um.insert({k, v});
+        tend = std::chrono::steady_clock::now();
         umit += tend - tstart;
         if (!success) {
             std::cout << "um.insert failed\n" << std::endl;
@@ -128,7 +180,7 @@ int main(int argc, char **argv) {
     // dump umap
     std::cout << "*** umap ***\n";
     for (auto& [k, v] : um) {
-        std::cout << "um[" << k << "]: " << v << std::endl;
+        std::cout << "um[" << k.k_key << "]: " << v.v_value << std::endl;
     }
 #endif
     
@@ -136,7 +188,7 @@ int main(int argc, char **argv) {
     //std::cout << "umaps: starting lookups\n";
     for (int i = 0; i < num; i++) {
         auto bno = rand() % keys.bucket_count();
-        while (!keys.bucket_size(bno)) {
+        while (!keys.bucket_size(bno)) { // find a non-empty bucket
             bno++;
             if (bno >= keys.bucket_count())
                 bno = 0;
@@ -144,9 +196,9 @@ int main(int argc, char **argv) {
         auto it = keys.begin(bno);
         std::advance(it, rand() % keys.bucket_size(bno));
         //std::cout << "looking up key " << *it << "\n";
-        tstart = std::chrono::high_resolution_clock::now();
+        tstart = std::chrono::steady_clock::now();
         auto record = um.find(*it);
-        tend = std::chrono::high_resolution_clock::now();
+        tend = std::chrono::steady_clock::now();
         umlt += tend - tstart;
         if (record == um.end()) {
             std::cout << "um.find failed\n" << std::endl;
@@ -157,7 +209,7 @@ int main(int argc, char **argv) {
     //std::cout << "maps: done lookups\n";
     avg = umlt / num;
     std::cout << "umaps lookup time: " << umlt.count() << "ms" << " avg: " << avg.count() << "us\n";
-    
+
 out:
     return err;
 }
